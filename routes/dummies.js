9e013e271d798617fec2b6e7365442407c20aa73
Scott Lind <scttlnd@gmail.com>
2021-09-08 13:27:11 +0200

    admin route

--- routes/dummies.js
+++ routes/dummies.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/authenticate');
+const admin = require('../middleware/admin');
 
 
 router.get('/public', (req, res) => {
@@ -10,6 +11,10 @@ router.get('/public', (req, res) => {
 
 router.get('/private', [auth], (req, res) => { // [auth, ...] middleware is set her. just use an array with one item if only one piece of middleware
     res.send(JSON.stringify({message: 'This is a private endpoint. Only members allowed yo.', account: req.account})); // req.account exists becuase it "went through" the auth
+});
+ 
+router.get('/adminonly', [auth, admin], (req, res) => {
+    res.send(JSON.stringify({message: 'This is for admins only yo.', account: req.body}));
 });
 
 module.exports = router;
